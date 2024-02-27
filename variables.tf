4fe8b52c8a50570617a689f7c5d56c562c53938e
adnanDatad <156879391+adnanDatad@users.noreply.github.com>
2024-02-27 12:32:07 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "enviroinment" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "subscription_id" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
+variable "region_code_name"{
+    type = string
+}
+variable "rg_name"{
+    type = string
+}
+
+#ADF
+variable "adf_name"{
+    type = string
+}
+
+#ADB
+variable "adb_name"{
+    type = string
+}
+variable "databricks_sku"{
+    type = string
+}
+
+#ADLS
